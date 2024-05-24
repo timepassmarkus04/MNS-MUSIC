a6c0a11df2b59a16497a4af07fd587b5a4705d76
Rajputshivsingh65 <166673972+Rajputshivsingh65@users.noreply.github.com>
2024-05-24 23:10:46 +0530

    Update autobroadcast.py

--- VIPMUSIC/cplugin/autobroadcast.py
+++ VIPMUSIC/cplugin/autobroadcast.py
@@ -7,9 +7,9 @@ from config import START_IMG_URL, AUTO_GCAST_MSG, AUTO_GCAST, LOGGER_ID
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Convert AUTO_GCAST to boolean based on "On" or "Off"
-AUTO_GCASTS = AUTO_GCAST.strip().lower() == "on"
+AUTO_GCASTS = AUTO_GCAST.strip().lower() == "off"
 
-START_IMG_URLS = "https://graph.org/file/760169f7f8dd536c50793.jpg"
+START_IMG_URLS = "https://telegra.ph/file/8be064de4663788e86535.jpg"
 
 MESSAGES = f"""**𝗟𝗼𝗼𝗸𝗶𝗻𝗴 𝗙𝗼𝗿 𝗔𝗴𝗲𝗻𝘁 𝗪𝗼𝗿𝗸 𝗜𝗻 𝗡𝗲𝘄 𝗣𝗹𝗮𝘁𝗳𝗼𝗿𝗺 𝗝𝘂𝘀𝘁 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗠𝗲 𝗪𝗵𝗼 𝗪𝗮𝗻𝘁 𝘁𝗼 𝗪𝗼𝗿𝗸 𝗔𝘀 𝗔 𝗔𝗴𝗲𝗻𝘁.
 
@@ -45,7 +45,7 @@ BUTTON = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(
                 "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏",
-                url=f"https://t.me/TG_VC_BOT?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
             )
         ]
     ]
