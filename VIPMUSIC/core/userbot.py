5ad5552234bcec7fd1a27cf4c6b95645e4994d5c
Rajputshivsingh65 <166673972+Rajputshivsingh65@users.noreply.github.com>
2024-05-24 22:58:12 +0530

    Update userbot.py

--- VIPMUSIC/core/userbot.py
+++ VIPMUSIC/core/userbot.py
@@ -65,10 +65,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
+                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("Dns_Official_Channel")
+                await self.one.join_chat("Dns_Official_Channel")
 
             except:
                 pass
@@ -95,10 +95,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("THE_VIP_BOY")
-                await self.two.join_chat("THE_VIP_BOY_OP")
-                await self.two.join_chat("TG_FRIENDSS")
-                await self.two.join_chat("VIP_CREATORS")
+                await self.two.join_chat("Dns_Official_Channel")
+                await self.two.join_chat("Dns_Official_Channel")
+                await self.two.join_chat("Dns_Official_Channel")
+                await self.two.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(2)
@@ -119,10 +119,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("THE_VIP_BOY")
-                await self.three.join_chat("THE_VIP_BOY_OP")
-                await self.three.join_chat("TG_FRIENDSS")
-                await self.three.join_chat("VIP_CREATORS")
+                await self.three.join_chat("Dns_Official_Channel")
+                await self.three.join_chat("Dns_Official_Channel")
+                await self.three.join_chat("Dns_Official_Channel")
+                await self.three.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(3)
@@ -144,10 +144,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("THE_VIP_BOY")
-                await self.four.join_chat("THE_VIP_BOY_OP")
-                await self.four.join_chat("TG_FRIENDSS")
-                await self.four.join_chat("VIP_CREATORS")
+                await self.four.join_chat("Dns_Official_Channel")
+                await self.four.join_chat("Dns_Official_Channel")
+                await self.four.join_chat("Dns_Official_Channel")
+                await self.four.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(4)
@@ -169,10 +169,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("THE_VIP_BOY")
-                await self.five.join_chat("THE_VIP_BOY_OP")
-                await self.five.join_chat("TG_FRIENDSS")
-                await self.five.join_chat("dhhdshhss6")
+                await self.five.join_chat("Dns_Official_Channel")
+                await self.five.join_chat("Dns_Official_Channel")
+                await self.five.join_chat("Dns_Official_Channel")
+                await self.five.join_chat("Dns_Official_Channel")
             except:
                 pass
             assistants.append(5)
