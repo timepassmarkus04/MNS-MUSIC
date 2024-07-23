89505d03c0c9cb31bcd1ced2095b8f88fd1924e1
Rajputshivsingh65 <166673972+Rajputshivsingh65@users.noreply.github.com>
2024-07-23 08:50:07 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -58,7 +58,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-photo_url = "https://telegra.ph/file/20b4a9fd06ea4a9457a61.jpg"
+photo_url = "https://telegra.ph/file/76b4278926229eebcc8ac.jpg"
 
 
 @app.on_message(
