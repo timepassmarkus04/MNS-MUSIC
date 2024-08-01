428da19f21543b75b2aea8689f49dcd1155d1e97
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-08-01 20:46:48 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -58,7 +58,7 @@ async def userdel(client, message: Message, _):
         await message.reply_text(_["sudo_8"])
 
 
-photo_url = "https://telegra.ph/file/76b4278926229eebcc8ac.jpg"
+photo_url = "https://telegra.ph/file/d3417b8e6a74daa337b64.jpg"
 
 
 @app.on_message(
