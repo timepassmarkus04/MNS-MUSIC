c9a180aade3bd89ad892a3c27af9b2f886cc6d68
timepassmarkus04 <markusmarwin2002@gmail.com>
2024-08-07 00:45:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_ID = 29350132
 API_HASH = "e854995be05edb5bf21f5b84bdc0212f"
 BOT_TOKEN = "7324085779:AAGt76PShYAXXtxIVBy51ZOXOY8wM0jr2FE"
 MONGO_DB_URI = "mongodb+srv://devilkingp0404:devilkingp0404@cluster0.iw04afs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 500))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002222142844"))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002222142844"))
 OWNER_ID = 7245959664
